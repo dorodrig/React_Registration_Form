40352e3c4ccc8a62dd9a53cc6c7ca65ace5cb826
dorodrig <dorodriguez145@misena.edu.co>
2023-11-27 20:25:42 -0500

    React: Ajustando el SASS del componente educational information

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,25 +1,62 @@
 import { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
+
 const SignIn = () => {
   let { setStep } = useContext(AuthContext);
+
+  // Datos del formulario
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // Focus de los inputs del formulario
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [passwordFocused, setPasswordFocused] = useState(false);
+
+  const handlerSubmit = (e) => {
+    // Evita el envío de datos a un backend
+    e.preventDefault();
+    console.log("Sending data to backend...");
+    console.log(`El usuario digitó: ${email} y ${password}`);
+  };
+
   return (
     <main>
       <h3>Hello, friend!</h3>
       <div className="card">
-        <form autoComplete="off">
+        <form onSubmit={handlerSubmit} autoComplete="off">
           <fieldset>
             <label htmlFor="email">email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              autoFocus
+              required
+              onBlur={() => setEmailFocused(true)}
+              data-focused={emailFocused}
+            />
             <p className="error">Valid email required</p>
           </fieldset>
           <fieldset>
             <label htmlFor="password">password:</label>
-            <input type="password" id="password" required />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="password"
+              pattern="(?=.*[\W])(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              required
+              onBlur={() => setPasswordFocused(true)}
+              data-focused={passwordFocused}
+            />
             <p className="error">Valid password required</p>
           </fieldset>
           <button type="submit" onClick={() => setStep("personal")}>
             login
           </button>
+          {/* <div className="forgot" onClick={() => setStep("forgot")}>
+            Forgot password?
+          </div> */}
         </form>
       </div>
       <p>
