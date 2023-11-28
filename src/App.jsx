1349e5759aa3f779e62845c05584bc833dada70c
dorodrig <dorodriguez145@misena.edu.co>
2023-11-27 21:06:34 -0500

    React: Se crea la primera version el componente Work Experience

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import SignUp from "./components/SignUp";
 import Personal_Information from "./components/Personal_Information";
 import Educational_Information from "./components/Educational_Information";
 import SignIn from "./components/SignIn";
+import Work_Experience from "./components/Work_Experience";
 
 function App() {
-  const [step, setStep] = useState("signin");
+  const [step, setStep] = useState("work");
   return (
     <>
       <AuthContext.Provider value={{ step, setStep }}>
@@ -15,6 +16,7 @@ function App() {
           {step === "personal" && <Personal_Information />}
           {step === "educational" && <Educational_Information />}
           {step === "signin" && <SignIn />}
+          {step === "work" && <Work_Experience />}
         </div>
       </AuthContext.Provider>
     </>
