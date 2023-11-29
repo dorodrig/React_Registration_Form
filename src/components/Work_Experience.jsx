12d80f0ee1d7b148a890d8eab8aa032906da1546
dorodrig <dorodriguez145@misena.edu.co>
2023-11-28 21:21:13 -0500

    React: Se finaliza el componente work experience

--- src/components/Work_Experience.jsx
+++ src/components/Work_Experience.jsx
@@ -4,60 +4,65 @@ const Work_Experience = () => {
       <h2>Work Experince</h2>
       <form autoComplete="off">
         <fieldset>
-          <div className="educational_container">
-            <div className="educational_container__title">
-              <label htmlFor="title">Qualification</label>
+          <div className="work_container">
+            <div className="work_container__title">
+              <label htmlFor="title">Job title</label>
               <input type="text" name="Qualification" id="Qualification" />
             </div>
-            <div className="educational_container__country">
-              <label htmlFor="country">Country</label>
+            <div className="work_container__country">
+              <label htmlFor="country">Hierarchy</label>
               <select name="droplist_country" id="droplist_country">
                 <option value="seleccion">Seleccione</option>
-                <option value="Colombia">Colombia</option>
-                <option value="Ecuador">Ecuador</option>
-                <option value="Peru">Peru</option>
+                <option value="Colombia">Junior</option>
+                <option value="Ecuador">Semi-Senior</option>
+                <option value="Peru">Senior</option>
               </select>
             </div>
-            <div className="educational_container__institution">
-              <label htmlFor="institution">Institution</label>
+            <div className="work_container__institution">
+              <label htmlFor="institution">company name</label>
               <input type="text" name="institution" id="institution" />
             </div>
           </div>
         </fieldset>
         <fieldset>
-          <div className="container-status">
-            <div className="Study_level">
-              <label htmlFor="Study_level">Study level</label>
-              <select name="Study_level" id="droplist_Study_level">
+          <div className="work_container__status">
+            <div className="work_container__Studylevel">
+              <label htmlFor="Study_level">Contry</label>
+              <select name="Study_level" id="work_container_droplist">
                 <option value="Seleccione"> Seleccione</option>
-                <option value="Secondary">Secondary</option>
-                <option value="Technique">Technique</option>
-                <option value="academic">Aademic</option>
-                <option value="postgraduate">Postgraduate</option>
-                <option value="master">Master</option>
-                <option value="Doctorate">Doctorate</option>
+                <option value="Argentina">Argentina</option>
+                <option value="Brasil">Brasil</option>
+                <option value="Colombia">Colombia</option>
+                <option value="Ecuador">Ecuador</option>
+                <option value="Peru">Peru</option>
+                <option value="Venezuele">Venezuele</option>
                 <option value="Otro">Otro</option>
               </select>
               <p className="error">Required</p>
             </div>
-            <div className="container-status__status">
-              <label htmlFor="status">Status</label>
-              <select name="droplist_status" id="droplist_status">
-                <option value="seleccion">Seleccione</option>
-                <option value="Graduado">Graduado</option>
-                <option value="Estudiante">Estudiante</option>
-                <option value="Egresado">Egresado</option>
-              </select>
+            <div className="work_container__industry">
+              <label htmlFor="status">Type of industry</label>
+              <input
+                type="text"
+                name="work_container__typeindustry"
+                id="typeindustry"
+              />
             </div>
-            <div className="container-status__finalized">
-              <label htmlFor="finalized">Finalized</label>
-              <select name="finalizes__year" id="finalizes__year">
-                <option value="seleccione">seleccione </option>
-                <option value="2023">2023</option>
-                <option value="2022">2022</option>
-                <option value="2021">2021</option>
-                <option value="2020">2020</option>
-              </select>
+            <div className="work_container__wcfinalized">
+              <label htmlFor="finalized">Work area</label>
+              <input type="text" name="finalizes__year" id="finalizes__year" />
+            </div>
+          </div>
+        </fieldset>
+        <fieldset>
+          <div className="datework">
+            <div className="start">
+              <label htmlFor="start">Start date</label>
+              <input type="date" name="start" id="start" />
+            </div>
+            <div className="end">
+              <label htmlFor="end">End date</label>
+              <input type="date" name="end" id="end" />
             </div>
           </div>
         </fieldset>
